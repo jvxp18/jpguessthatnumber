699c5c07d494b380bc76ec3d0e2420a15ee29478
jvxp18 <jvxp18@gmail.com>
2020-07-10 21:25:22 +0000

    Challenge

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,28 +8,28 @@ echo "Fantastic, $name, please have a seat if you haven't so already! It's time 
 echo "Please give me an integer"
 read guess
 	
-	while [[ ! $guess =~ ^[0-9]+$ ]]
+	while [[ ! ${guess#0} =~ ^[0-9]+$ ]] || [[ ${guess#0} > 2147483647 ]]
 	do
-		echo "positive integers only. Try again"
+		echo "positive integers and only. Try again"
 		read guess
 	done
 
 files=$(ls -1bA | wc -l)
 	
 
-	while [[ $guess -ne $files ]]
+	while [[ ${guess#0} -ne $files ]]
 
 	do
-		if [[ $guess -gt $files ]]
+		if [[ ${guess#0} -gt $files ]]
 		then 
 			echo "Sorry friend, that number is high! Try again!"	
-		elif [[ $guess -lt $files ]]
+		elif [[ ${guess#0} -lt $files ]]
 		then
 			echo "Too low! Again!"
 		fi
 		read guess
 
-		while [[ ! $guess =~ ^[0-9]+$ ]]
+		while [[ ! ${guess#0} =~ ^[0-9]+$ ]]
 		do
 			echo "positive integers only. try again"
 			read guess
